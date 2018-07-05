5268034282aeb7e676e60988bf1db748104c6392
Vilhena Sorrentino, Ian <Ian.Sorrentino@gft.com>
2018-07-05 10:16:17 -0300

    Initial Commit

---
+++ testeURLACESSO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testeURLACESSO
+//
+//  Created by Vilhena Sorrentino, Ian on 05/07/18.
+//  Copyright © 2018 Vilhena Sorrentino, Ian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
